d35485325c3bcdf30f558708684454e12333f839
big stepper <rmqwerty@mail.ru>
2025-01-18 22:45:51 +0300

    Initial Commit

---
+++ TravelSchedule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelSchedule
+//
+//  Created by big stepper on 18.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by big stepper on 18.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
